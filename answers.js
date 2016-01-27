0f52365a763ad5a1ec6493a8e2256c9440fbaf70
dreboom <drestorelli@gmail.com>
2016-01-27 19:54:10 +0000

    first exercise

--- answers.js
+++ answers.js
@@ -1,1 +1,15 @@
-hello
+var str = "hello";
+
+var firstChar = function(arg) {
+    return arg.charAt(0);
+}
+
+var firstLetter = firstChar(str);
+
+var message = firstLetter + " is the first letter of " + str;
+
+console.log(firstLetter);
+
+console.log(str);
+
+console.log(message);
